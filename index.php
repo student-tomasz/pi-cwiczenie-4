bc9b38e42a12b05823d42a406fd3b3601675191c
Tomasz Cudziło <tomasz@luzid.co>
2012-01-01 19:07:30 +0100

    add action links to the files listing

--- index.php
+++ index.php
@@ -3,12 +3,19 @@ include('db_connect.php');
 
 $files = mysql_query("SELECT * FROM {$config['db_table_name']}");
 while($file = mysql_fetch_array($files)) {
+  $id = $file['id'];
   echo "<tr>";
-  echo "<td>{$file['id']}</td>";
-  echo "<td>{$file['name']}</td>";
-  echo "<td>{$file['type']}</td>";
-  printf("<td>%.2d kB</td>", $file['size']/1000);
-  echo "<td>&nbsp;</td>";
+  printf("<td class='id'>%s</td>",        $file['id']);
+  printf("<td class='name'>%s</td>",      $file['name']);
+  printf("<td class='type'>%s</td>",      $file['type']);
+  printf("<td class='size'>%.2d kB</td>", $file['size']/1000);
+  echo "  <td>";
+  echo "    <ul class='actions'>";
+  echo "      <li class='action download'><a href='download.php?id={$id}' onclick='downloader.download({$id}); return false;'>pobierz</a></li>";
+  echo "      <li class='action convert'><a href='convert.php?id={$id}' onclick='converter.convert({$id}); return false;'>konwertuj</a></li>";
+  echo "      <li class='action destroy'><a href='destroy.php?id={$id}' onclick='destroyer.destroy({$id}); return false;'>usuń</a></li>";
+  echo "    </ul>";
+  echo "  </td>";
   echo "</tr>\n";
 }
 
